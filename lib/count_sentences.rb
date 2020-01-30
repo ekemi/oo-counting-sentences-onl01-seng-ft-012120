6799c90e4c7a6ea3c6d2dc04bc9854f3e290fa1d
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-30 22:11:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,17 +4,43 @@ class String
 
   def sentence?
     
+ if self.end_with?(".") == true
+    return true
+
+else
+   false   
+
+  end  
+      
+
   end
 
   def question?
+       
+ if self.end_with?("?") == true
+    return true
+
+else
+   false   
+
+  end 
 
   end
 
   def exclamation?
+       
+ if self.end_with?("!") == true
+    return true
+
+else
+   false   
+
+  end 
 
   end
 
   def count_sentences
-
+    
+   self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
 end
